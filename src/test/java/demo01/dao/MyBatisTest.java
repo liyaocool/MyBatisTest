50d70b1b56c849bd34b76326cf0d4433c3cabcd0
liyaocool <crazyliyao@163.com>
2019-09-05 09:37:34 +0800

    配置项目架构

---
+++ src/test/java/demo01/dao/MyBatisTest.java
@@ -0,0 +1,18 @@
+package demo01.dao;
+
+
+import org.junit.Test;
+
+
+public class MyBatisTest {
+    @Test
+    public void test(){
+        //1.读取配置文件
+
+        //2.创建SqlSessionFactory工厂
+        //3.使用工厂生产SqlSession对象
+        //4.使用SqlSession创建Dao接口的代理对象
+        //5.使用代理对象执行方法
+        //6.释放资源
+    }
+}
